3279e1890c90604908d9671263950a960eb41c0e
Hansoo Lee <dlgkstn5678@gamil.com>
2022-12-23 18:18:09 +0900

    add component sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,1 +1,23 @@
-// this is gonna be sidebar :)
+import { Link } from "react-router-dom";
+
+const SideBar = () => {
+    return (
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <Link class="nav-link active" aria-current="page" to="#">Sports/Activity</Link>
+            </li>
+            <li class="nav-item">
+                <Link class="nav-link" to="#">Trip/Going Out</Link>
+            </li>
+            <li class="nav-item">
+                <Link class="nav-link" to="#">Study Groups</Link>
+            </li>
+            <li class="nav-item">
+                <Link class="nav-link" to="#">Flashmob</Link>
+            </li>
+        </ul>
+
+    );
+};
+
+export default SideBar;
