2c7ae34cca1634bd7244a8ad46238232b3791f6d
fastwon <cbkh7@naver.com>
2022-12-28 17:38:23 +0900

    Table name 'user' change to 'users'

--- src/main/java/com/iljo/userserver/jpa/UserEntity.java
+++ src/main/java/com/iljo/userserver/jpa/UserEntity.java
@@ -3,14 +3,15 @@ package com.iljo.userserver.jpa;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 /**
  *  서비스를 이용하는 유저에 대한 entity
  * */
 @Data
 @Entity
-@Table(name = "user")
-public class UserEntity {
+@Table(name = "users")
+public class UserEntity implements Serializable {
 
     /**
      * 유저의 닉네임이자 primary key
@@ -21,9 +22,6 @@ public class UserEntity {
 
     @Column(nullable = false)
     private String password;
-
-//    @Column(nullable = false)
-//    private String encrypted_password;
 
     @Column(nullable = false, length = 6)
     private String name;
