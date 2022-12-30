dbdf38e899b79f9c8757973a15c983beebc7082b
SungHwan Lim <sunghwan644@gmail.com>
2022-12-30 17:10:56 +0900

    Update MainPage

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Router, Routes, Route, BrowserRouter } from "react-router-dom";
-import NavigationBar from "./components/NavigationBar";
+import {Routes, Route} from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-
-import Profile from "./components/Profile";
-import ProfileMini from "./components/ProfileMini";
-import Pagination from "./components/Pagination";
-import SortList from "./components/SortList";
-
-import SideBarMain from "./components/SideBarMain";
-import SideBarRoom from "./components/SideBarRoom";
-import RoomArticle from "./components/RoomArticle";
-import RoomComment from "./components/RoomComment";
-import SimpleRoomParticipants from "./components/SimpleRoomParticipants";
-import SimpleRoomPicture from "./components/SimpleRoomPicture";
-import SimpleRoomText from "./components/SimpleRoomText";
-import MainContents from "./components/MainContents";
-import JoinRoomButton from "./components/JoinRoomButton";
-
-import Login from "./Pages/Login";
-import SignUp from "./Pages/SignUp";
-import SignUp2 from "./Pages/SignUp2";
-import CreateRoom from "./Pages/CreateRoom";
-import MyPage2 from "./components/MyPage2";
 import Routers from "./Routers/Routers";
 function App() {
   return (
--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,11 +1,70 @@
 import React from 'react'
-
+import MainArticle from '../components/MainArticle';
+import MainArticle2 from '../components/MainArticle2';
+import $ from "jquery";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 const Main = () => {
+
+  useEffect(() => {
+    AOS.init();
+  })
+
+  const imgSrcList = [
+    {
+      path: "https://assets.entrepreneur.com/content/3x2/2000/20180507200150-GettyImages-639336736.jpeg",
+      text: "우리는 현재 더불어서 살아가고 있습니다.",
+    },
+    {
+      path: "https://t1.daumcdn.net/cfile/blog/99D6A3425CF9225B06",
+      text: "그럼 이건 어떠세요?",
+    },
+    {
+      path: "https://src.hidoc.co.kr/image/lib/2022/2/11/1644569794564_0.jpg",
+      text: "여기서 새로운 모임을 가져보세요!",
+    },
+  ];
+
+  // $(window).scroll(function(){
+
+  //   var height = $(window).scrollTop();
+    
+  //   var y =  -1/500 * height + 115/50;
+  //   $('.main-total').eq(0).css('opacity', y);
+  
+  //   console.log(height);
+
+  //   var z = (-1/5000) * height + 565/500;
+  //   $('.main-total').eq(0).css('transform', `scale( ${z} )`);
+  // });
   return (
     <div>
-    <div>Main</div>
+        <div>
+            <div data-aos="fade-up" className='Title-Article'>
+              MO-INEE
+            </div>
+          </div>
+
+      {imgSrcList.map((imgSrc, index) =>
+        index % 2 === 0 ? (
+          <div data-aos="fade-right" className="main-total">
+            <MainArticle 
+            path = {imgSrc.path}
+            text = {imgSrc.text}
+            />
+          </div>
+        ) : (
+          <div data-aos="fade-left" className="main-total">
+            <MainArticle2 
+            path = {imgSrc.path}
+            text = {imgSrc.text}
+            />
+          </div>
+        )
+      )}
     </div>
-  )
+  );
 }
 
 export default Main;
