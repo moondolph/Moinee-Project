333ec8dba49f6441ce0ae93af51059660f64855e
fastwon <cbkh7@naver.com>
2023-01-05 17:54:38 +0900

    asdf

--- src/App.js
+++ src/App.js
@@ -2,24 +2,43 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+    //  fileChangedHandler = (event) => {
+    //   const files = event.target.files;
+    //   this.setState({
+    //     selectedFiles: files
+    //   });
+    // };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="page container">
+        <div className="titleWrap">※프로필수정※</div><br />
+
+        <div className="contentWrap">
+          <div className="inputTitle">프로필사진</div><br />
+
+          <input type="file" multiple /><br />
+          userId : <input type="text" value="abc667" /> <br />
+          password : <input type="text" value="123asd123" /> <br />
+          name : <input type="text" value="abc667" /> <br />
+          birthday : <input type="text" value="11111111" /> <br />
+          email : <input type="text" value="abc667@naver.com" /> <br />
+          gender : <input type="text" value="M" /> <br />
+          address : <input type="text" value="abc6671111" /> <br />
+          latitude : <input type="text" value="123.2211" /> <br />
+          longitude : <input type="text" value="52.1455" /> <br />
+
+          <button >저장하기</button>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+// onChange={this.fileChangedHandler}
+
+// onClick={this.onClickHandler}
