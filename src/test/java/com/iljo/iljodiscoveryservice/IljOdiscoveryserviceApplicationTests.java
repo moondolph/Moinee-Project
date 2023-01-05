0b5226d1af8056cecc3b35219fdc27c2b068eb65
SeoulPolarBear <sungh9696@naver.com>
2023-01-05 14:45:43 +0900

    Initial commit

---
+++ src/test/java/com/iljo/iljodiscoveryservice/IljOdiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iljo.iljodiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IljOdiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
