90690f3dfa4ea387145fab281fcf9613862b08bb
Whiteheim <gns240@gmail.com>
2023-01-12 14:54:15 +0900

    GateWay_heim_edit

--- src/main/java/com/iljo/iljoapigateway/IljoApiGatewayApplication.java
+++ src/main/java/com/iljo/iljoapigateway/IljoApiGatewayApplication.java
@@ -3,6 +3,9 @@ package com.iljo.iljoapigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +15,13 @@ public class IljoApiGatewayApplication {
 		SpringApplication.run(IljoApiGatewayApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
