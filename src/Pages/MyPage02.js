fcd649dc529dd87739ff73eaad509657ca3e873f
SeoulPolarBear <sungh9696@naver.com>
2023-01-16 13:28:04 +0900

    change

--- src/Pages/MyPage02.js
+++ src/Pages/MyPage02.js
@@ -35,7 +35,7 @@ export default function MyPage02() {
     const [evaluation, setEvaluation] = useState(0);
     // 서버랑 연결할 때는, props에서 userId 꺼내서 URI에 넣어주어야 한다.
     const getUser = async () => {
-        await axios.get("http://localhost:3001/user").then((response) => {
+        await axios.get("http://34.68.3.131:8000/user/abeathem1e").then((response) => {
             console.log("방 간략정보에서 방장 프로필 가져옴");
             console.log(response.data);
             setUser(response.data);
